5f45df3f3d01bd056a7027d4180800703d48b63b
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-06-18 14:25:43 +0900

    Delete duplicated extract_aab_apk.sh

--- extract_aab_apk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-java -jar ./bundletool-all-1.16.0.jar build-apks --bundle=../app/build/outputs/bundle/release/app-release.aab --output=app-release.apks --mode=universal
-unzip -o app-release.apks -d app-release
