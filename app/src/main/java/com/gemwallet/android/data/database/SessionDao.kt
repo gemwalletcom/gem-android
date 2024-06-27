c58a609c44428f847bdd8e4fff2a259bce2688a7
furenster <123000419+furenster@users.noreply.github.com>
2024-06-26 20:14:47 -0700

    Add session dao

---
+++ app/src/main/java/com/gemwallet/android/data/database/SessionDao.kt
@@ -0,0 +1,23 @@
+package com.gemwallet.android.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.gemwallet.android.data.database.entities.DbSession
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SessionDao {
+    @Query("SELECT * FROM session WHERE id = 1")
+    fun session(): Flow<DbSession?>
+
+    @Query("SELECT * FROM session WHERE id = 1")
+    fun getSession(): DbSession?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(session: DbSession): Boolean
+
+    @Query("DELETE FROM session")
+    suspend fun clear()
+}
