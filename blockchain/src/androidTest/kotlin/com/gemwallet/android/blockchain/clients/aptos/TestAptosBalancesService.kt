3d32cd62c1e4930c6385a7cabc1badc59d140ecf
furenster <123000419+furenster@users.noreply.github.com>
2024-12-15 23:33:33 -0800

    Add aptos token balances

---
+++ blockchain/src/androidTest/kotlin/com/gemwallet/android/blockchain/clients/aptos/TestAptosBalancesService.kt
@@ -0,0 +1,67 @@
+package com.gemwallet.android.blockchain.clients.aptos
+
+import com.gemwallet.android.blockchain.clients.aptos.services.AptosBalancesService
+import com.wallet.core.blockchain.aptos.models.AptosResource
+import com.wallet.core.blockchain.aptos.models.AptosResourceBalance
+import com.wallet.core.blockchain.aptos.models.AptosResourceBalanceOptional
+import com.wallet.core.blockchain.aptos.models.AptosResourceCoin
+
+class TestAptosBalancesService(
+    val nativeBalance: String? = null,
+) : AptosBalancesService {
+
+    var nativeRequest: String? = null
+    var tokenRequest: String? = null
+
+    override suspend fun balance(address: String): Result<AptosResource<AptosResourceBalance>> {
+        nativeRequest = address
+        nativeBalance ?: return Result.failure(Exception())
+        return Result.success(
+            AptosResource(
+                type = "",
+                data = AptosResourceBalance(
+                    coin = AptosResourceCoin(
+                        value = nativeBalance
+                    )
+                )
+            )
+        )
+    }
+
+    override fun resource(address: String, resource: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun resources(address: String): Result<List<AptosResource<AptosResourceBalanceOptional>>> {
+        tokenRequest = address
+        return Result.success(
+            listOf(
+                AptosResource(
+                    type = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>",
+                    AptosResourceBalanceOptional(
+                        AptosResourceCoin(
+                            "10000000"
+                        )
+                    )
+                ),
+                AptosResource(
+                    type = "0x1::coin::CoinStore<0xe4ccb6d39136469f376242c31b34d10515c8eaaa38092f804db8e08a8f53c5b2::assets_v1::EchoCoin002>",
+                    AptosResourceBalanceOptional(
+                        AptosResourceCoin(
+                            "30000000"
+                        )
+                    )
+                ),
+                AptosResource(
+                    type = "0x1::coin::CoinStore<0x159df6b7689437016108a019fd5bef736bac692b6d4a1f10c941f6fbb9a74ca6::oft::CakeOFT>",
+                    AptosResourceBalanceOptional(
+                        AptosResourceCoin(
+                            "50000000"
+                        )
+                    )
+                ),
+
+            )
+        )
+    }
+}
--- blockchain/src/main/java/com/gemwallet/android/blockchain/clients/aptos/services/AptosBalancesService.kt
+++ blockchain/src/main/java/com/gemwallet/android/blockchain/clients/aptos/services/AptosBalancesService.kt
@@ -2,10 +2,17 @@ package com.gemwallet.android.blockchain.clients.aptos.services
 
 import com.wallet.core.blockchain.aptos.models.AptosResource
 import com.wallet.core.blockchain.aptos.models.AptosResourceBalance
+import com.wallet.core.blockchain.aptos.models.AptosResourceBalanceOptional
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface AptosBalancesService {
     @GET("/v1/accounts/{address}/resource/0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>")
     suspend fun balance(@Path("address") address: String): Result<AptosResource<AptosResourceBalance>>
+
+    @GET("/v1/accounts/{address}/resource/{resource}")
+    fun resource(@Path("address") address: String, @Path("resource") resource: String)
+
+    @GET("/v1/accounts/{address}/resources")
+    fun resources(@Path("address") address: String): Result<List<AptosResource<AptosResourceBalanceOptional>>>
 }
