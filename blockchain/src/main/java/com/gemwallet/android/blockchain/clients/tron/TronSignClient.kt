c1b3fb54c4d6c4d53eb18b52ff8e18ecf6b6e622
furenster <123000419+furenster@users.noreply.github.com>
2025-01-09 20:11:52 -0800

    Implement tron stake signs

--- blockchain/src/main/java/com/gemwallet/android/blockchain/clients/tron/TronSignClient.kt
+++ blockchain/src/main/java/com/gemwallet/android/blockchain/clients/tron/TronSignClient.kt
@@ -4,7 +4,6 @@ import android.text.format.DateUtils
 import com.gemwallet.android.blockchain.clients.SignClient
 import com.gemwallet.android.ext.type
 import com.gemwallet.android.math.decodeHex
-import com.gemwallet.android.model.ConfirmParams
 import com.gemwallet.android.model.SignerParams
 import com.gemwallet.android.model.TxSpeed
 import com.google.protobuf.ByteString
@@ -18,6 +17,8 @@ import java.math.BigInteger
 class TronSignClient(
     private val chain: Chain,
 ) : SignClient {
+
+    override fun supported(chain: Chain): Boolean = this.chain == chain
 
     override suspend fun signTransfer(
         params: SignerParams,
@@ -55,26 +56,59 @@ class TronSignClient(
         val signingOutput = AnySigner.sign(signInput, CoinType.TRON, Tron.SigningOutput.parser())
         return signingOutput.json.toByteArray()
     }
-    public fun signDelegate(params: SignerParams) {
-        val chainData = params.chainData as TronSignerPreloader.TronChainData
-        val votes = chainData.votes
+
+    fun signDelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+
         val freezContract = Tron.FreezeBalanceV2Contract.newBuilder().apply {
             this.ownerAddress = params.input.from.address
             this.frozenBalance = params.input.amount.toLong()
             this.resource = "BANDWIDTH"
+        }.build()
+        val voteContract = createVoteContract(params)
+        return listOf(
+            sign(params, freezContract, privateKey),
+            sign(params, voteContract, privateKey),
+        )
+    }
+
+    fun signUndelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+        val chainData = params.chainData as TronSignerPreloader.TronChainData
+        val votes = chainData.votes
+        return listOf(
+            Tron.UnfreezeBalanceV2Contract.newBuilder().apply {
+                this.ownerAddress = params.input.from.address
+                this.unfreezeBalance = params.input.amount.toLong()
+                this.resource = "BANDWIDTH"
+            }.build(),
+            if (votes.isEmpty())  null else createVoteContract(params),
+        )
+        .filterNotNull()
+        .map {
+            sign(params, it, privateKey)
         }
-        val vooteContract = Tron.VoteWitnessContract.newBuilder().apply {
+    }
+
+    fun signRedelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+        val voteContract = createVoteContract(params)
+        return listOf(sign(params, voteContract, privateKey))
+    }
+
+    fun rewards(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+        val contract = Tron.WithdrawBalanceContract.newBuilder().apply {
             this.ownerAddress = params.input.from.address
-            this.support = true
-            this.addAllVotes(
-                votes.map {
-                    Tron.VoteWitnessContract.Vote.newBuilder().apply {
-                        this.voteAddress = it.key
-                        this.voteCount = it.value
-                    }.build()
-                }
-            )
-        }
+        }.build()
+        return listOf(
+            sign(params, contract, privateKey)
+        )
+    }
+
+    fun withdraw(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+        val contract = Tron.WithdrawExpireUnfreezeContract.newBuilder().apply {
+            this.ownerAddress = params.input.from.address
+        }.build()
+        return listOf(
+            sign(params, contract, privateKey)
+        )
     }
 
     private fun getTransferContract(value: BigInteger, ownerAddress: String, recipient: String)
@@ -92,5 +126,46 @@ class TronSignClient(
         this.amount = ByteString.copyFrom(value.toByteArray())
     }.build()
 
-    override fun supported(chain: Chain): Boolean = this.chain == chain
+    private fun createVoteContract(params: SignerParams) = Tron.VoteWitnessContract.newBuilder().apply {
+            this.ownerAddress = params.input.from.address
+            this.support = true
+            this.addAllVotes(
+                (params.chainData as TronSignerPreloader.TronChainData).votes.map {
+                    Tron.VoteWitnessContract.Vote.newBuilder().apply {
+                        this.voteAddress = it.key
+                        this.voteCount = it.value
+                    }.build()
+                }
+            )
+        }.build()
+
+    private fun sign(params: SignerParams, contract: Any, privateKey: ByteArray): ByteArray {
+        val blockInfo = params.chainData as TronSignerPreloader.TronChainData
+        val transaction = Tron.Transaction.newBuilder().apply {
+            when (contract) {
+                is Tron.TransferContract -> this.transfer = contract
+                is Tron.TransferTRC20Contract -> this.transferTrc20Contract = contract
+                is Tron.FreezeBalanceV2Contract -> this.freezeBalanceV2 = contract
+                is Tron.VoteWitnessContract -> this.voteWitness = contract
+            }
+            this.blockHeader = Tron.BlockHeader.newBuilder().apply {
+                this.number = blockInfo.number
+                this.parentHash = ByteString.copyFrom(blockInfo.parentHash.decodeHex())
+                this.timestamp = blockInfo.timestamp
+                this.version = blockInfo.version.toInt()
+                this.witnessAddress = ByteString.copyFrom(blockInfo.witnessAddress.decodeHex())
+                this.txTrieRoot = ByteString.copyFrom(blockInfo.txTrieRoot.decodeHex())
+            }.build()
+
+            this.expiration = blockInfo.timestamp + 10 * DateUtils.HOUR_IN_MILLIS
+            this.timestamp = blockInfo.timestamp
+            this.feeLimit = blockInfo.fee().amount.toLong()
+        }
+        val signInput = Tron.SigningInput.newBuilder().apply {
+            this.transaction = transaction.build()
+            this.privateKey = ByteString.copyFrom(privateKey)
+        }.build()
+        val signingOutput = AnySigner.sign(signInput, CoinType.TRON, Tron.SigningOutput.parser())
+        return signingOutput.json.toByteArray()
+    }
 }
