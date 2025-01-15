51042f61c57ca087bd398db7cab600a07f970913
furenster <123000419+furenster@users.noreply.github.com>
2025-01-14 19:09:13 -0800

    Clean space

--- app/src/main/java/com/gemwallet/android/features/stake/stake/views/StakeScene.kt
+++ app/src/main/java/com/gemwallet/android/features/stake/stake/views/StakeScene.kt
@@ -1,9 +1,7 @@
 package com.gemwallet.android.features.stake.stake.views
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
@@ -16,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.gemwallet.android.R
 import com.gemwallet.android.ext.claimed
 import com.gemwallet.android.features.assets.model.PriceUIState
@@ -55,11 +52,9 @@ fun StakeScene(
         title = stringResource(id = R.string.transfer_stake_title),
         onClose = onCancel,
     ) {
-        Spacer(modifier = Modifier.size(16.dp))
         Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
             LazyColumn(modifier = Modifier.fillMaxSize()) {
                 item {
-                    Spacer16()
                     SubheaderItem(title = uiState.title)
                     Table(
                         items = listOf(
