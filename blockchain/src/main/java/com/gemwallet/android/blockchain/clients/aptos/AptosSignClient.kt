09ca611a24388e5ebed059f54b5e237c2a030aac
furenster <123000419+furenster@users.noreply.github.com>
2025-02-05 01:18:44 -0800

    Clean code

--- blockchain/src/main/java/com/gemwallet/android/blockchain/clients/aptos/AptosSignClient.kt
+++ blockchain/src/main/java/com/gemwallet/android/blockchain/clients/aptos/AptosSignClient.kt
@@ -18,34 +18,7 @@ class AptosSignClient(
 ) : SignClient {
 
     val coinType = WCChainTypeProxy().invoke(chain)
-
-//    override suspend fun signTransaction(params: SignerParams, txSpeed: TxSpeed, privateKey: ByteArray): List<ByteArray> {
-//        val metadata = params.chainData as AptosSignerPreloader.AptosChainData
-//        val fee = metadata.gasFee()
-//        val signInput = Aptos.SigningInput.newBuilder().apply {
-//            this.chainId = 1
-//            when (params.input) {
-//                is ConfirmParams.Stake -> throw IllegalArgumentException()
-//                is ConfirmParams.SwapParams -> TODO()
-//                is ConfirmParams.TokenApprovalParams -> TODO()
-//                is ConfirmParams.TransferParams.Native -> this.transfer =
-//                is ConfirmParams.TransferParams.Token -> this.tokenTransferCoins = buildTokenCoinMessage(params)
-//            }
-//            this.expirationTimestampSecs = 3664390082
-//            this.gasUnitPrice = fee.maxGasPrice.toLong()
-//            this.maxGasAmount = fee.limit.toLong()
-//            this.sequenceNumber = metadata.sequence
-//            this.sender = params.input.from.address
-//            this.privateKey = ByteString.copyFrom(privateKey)
-//        }.build()
-//        val output = AnySigner.sign(signInput, coinType, Aptos.SigningOutput.parser())
-//        if (output.errorMessage.isNullOrEmpty()) {
-//            return listOf(output.json.toByteArray())
-//        } else {
-//            throw Exception(output.errorMessage)
-//        }
-//    }
-
+    
     override suspend fun sign(
         params: ConfirmParams.TransferParams.Native,
         chainData: ChainSignData,
