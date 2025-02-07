b1d5ba68b346d2d912c999cbc8fc76c4ec5bdcf5
furenster <123000419+furenster@users.noreply.github.com>
2025-02-06 19:09:50 -0800

    Improve stellar signer

---
+++ blockchain/src/androidTest/kotlin/com/gemwallet/android/blockchain/clients/stellar/TestStellarSigner.kt
@@ -0,0 +1,69 @@
+package com.gemwallet.android.blockchain.clients.Stellar
+
+import com.gemwallet.android.blockchain.clients.near.NearSignClient
+import com.gemwallet.android.blockchain.clients.stellar.StellarSignClient
+import com.gemwallet.android.blockchain.clients.stellar.StellarSignPreloadClient
+import com.gemwallet.android.blockchain.includeLibs
+import com.gemwallet.android.blockchain.testPhrase
+import com.gemwallet.android.math.toHexString
+import com.gemwallet.android.model.ConfirmParams
+import com.gemwallet.android.model.DestinationAddress
+import com.gemwallet.android.model.Fee
+import com.gemwallet.android.model.TxSpeed
+import com.wallet.core.primitives.Account
+import com.wallet.core.primitives.AssetId
+import com.wallet.core.primitives.Chain
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import wallet.core.jni.CoinType
+import wallet.core.jni.HDWallet
+import java.math.BigInteger
+
+class TestStellarSigner {
+    companion object {
+        init {
+            includeLibs()
+        }
+    }
+
+    @Test
+    fun testNearNativeSign() {
+        val hdWallet = HDWallet(testPhrase, "")
+        val privateKey = hdWallet.getKeyForCoin(CoinType.STELLAR)
+        val from = hdWallet.getAddressForCoin(CoinType.STELLAR)
+        val signer = StellarSignClient(Chain.Stellar)
+
+        val sign = runBlocking {
+            signer.sign(
+                params = ConfirmParams.TransferParams.Native(
+                    AssetId(Chain.Stellar),
+                    Account(Chain.Stellar, from, ""),
+                    BigInteger.valueOf(10_000),
+                    DestinationAddress(from),
+                ),
+                chainData = StellarSignPreloadClient.StellarChainData(
+                    sequence = 1,
+                    fees = listOf(
+                        Fee(
+                            speed = TxSpeed.Normal,
+                            feeAssetId = AssetId(Chain.Stellar),
+                            amount = BigInteger.TEN
+                        ),
+                    ),
+                ),
+                finalAmount = BigInteger.valueOf(10_000),
+                TxSpeed.Normal,
+                privateKey.data(),
+            )
+        }
+
+        assertEquals("0x41414141414845774d46575a4d462b466b504d56665170332f702b71704e694f4f6851694" +
+                "4535466476756364774496f414141414367414141414141414141424141414141414141414141414" +
+                "14141424141414141414141414145414141414163544177565a6b775834575138785639436e662b6" +
+                "e36716b324934364643494e4a4e386142586f61306967414141414141414141414141414a7841414" +
+                "14141414141414141586f6130696741414142414b366e522b67544956464c7347454831776f42496" +
+                "252514f4f56334863745975742f4b7972316e4c62424d34527952674370327576664f7954354c326" +
+                "94d45507554797861726533515a446b544d62734756417142413d3d", sign.first().toHexString())
+    }
+}
--- blockchain/src/main/java/com/gemwallet/android/blockchain/clients/stellar/StellarSignClient.kt
+++ blockchain/src/main/java/com/gemwallet/android/blockchain/clients/stellar/StellarSignClient.kt
@@ -2,44 +2,48 @@ package com.gemwallet.android.blockchain.clients.stellar
 
 import com.gemwallet.android.blockchain.clients.SignClient
 import com.gemwallet.android.blockchain.operators.walletcore.WCChainTypeProxy
-import com.gemwallet.android.model.SignerParams
+import com.gemwallet.android.model.ChainSignData
+import com.gemwallet.android.model.ConfirmParams
 import com.gemwallet.android.model.TxSpeed
 import com.google.protobuf.ByteString
 import com.wallet.core.primitives.Chain
 import wallet.core.java.AnySigner
 import wallet.core.jni.StellarPassphrase
 import wallet.core.jni.proto.Stellar
+import java.math.BigInteger
 
 class StellarSignClient(
     private val chain: Chain
 ) : SignClient {
 
-    override suspend fun signTransaction(
-        params: SignerParams,
+    override suspend fun sign(
+        params: ConfirmParams.TransferParams.Native,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
         txSpeed: TxSpeed,
         privateKey: ByteArray
     ): List<ByteArray> {
-        val chainData = (params.chainData as? StellarSignPreloadClient.StellarChainData)
+        val chainData = (chainData as? StellarSignPreloadClient.StellarChainData)
             ?: throw Exception("bad params")
         val input = Stellar.SigningInput.newBuilder().apply {
             this.passphrase = StellarPassphrase.STELLAR.toString()
             this.fee = chainData.fee(txSpeed).amount.toInt()
             this.sequence = chainData.sequence
-            this.account = params.input.from.address
-            if (!params.input.memo().isNullOrEmpty()) {
+            this.account = params.from.address
+            if (!params.memo().isNullOrEmpty()) {
                 this.memoText = Stellar.MemoText.newBuilder().apply {
-                    this.text = params.input.memo()!!
+                    this.text = params.memo()!!
                 }.build()
             }
             if (chainData.fee(txSpeed).options.contains(StellarSignPreloadClient.StellarChainData.tokenAccountCreation)) {
                 this.opCreateAccount = Stellar.OperationCreateAccount.newBuilder().apply {
-                    this.destination = params.input.destination()!!.address
-                    this.amount = params.finalAmount.toLong()
+                    this.destination = params.destination().address
+                    this.amount = finalAmount.toLong()
                 }.build()
             } else {
                 this.opPayment = Stellar.OperationPayment.newBuilder().apply {
-                    this.destination = params.input.destination()!!.address
-                    this.amount = params.finalAmount.toLong()
+                    this.destination = params.destination().address
+                    this.amount = finalAmount.toLong()
                 }.build()
             }
             this.privateKey = ByteString.copyFrom(privateKey)
