9ac3034ca690538cfd94c41d585fdfb660d1a18d
furenster <123000419+furenster@users.noreply.github.com>
2025-02-09 20:25:22 -0800

    Tron signer improve

---
+++ blockchain/src/androidTest/kotlin/com/gemwallet/android/blockchain/clients/tron/TestTronSigner.kt
@@ -0,0 +1,195 @@
+package com.gemwallet.android.blockchain.clients.tron
+
+import com.gemwallet.android.blockchain.includeLibs
+import com.gemwallet.android.blockchain.testPhrase
+import com.gemwallet.android.math.toHexString
+import com.gemwallet.android.model.ConfirmParams
+import com.gemwallet.android.model.DestinationAddress
+import com.gemwallet.android.model.Fee
+import com.gemwallet.android.model.TxSpeed
+import com.wallet.core.primitives.Account
+import com.wallet.core.primitives.AssetId
+import com.wallet.core.primitives.Chain
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import wallet.core.jni.CoinType
+import wallet.core.jni.HDWallet
+import java.math.BigInteger
+
+class TestTronSigner {
+    companion object {
+        init {
+            includeLibs()
+        }
+    }
+
+    @Test
+    fun testTronNativeSign() {
+        val hdWallet = HDWallet(testPhrase, "")
+        val privateKey = hdWallet.getKeyForCoin(CoinType.TRON)
+        val from = hdWallet.getAddressForCoin(CoinType.TRON)
+        val signer = TronSignClient(Chain.Tron)
+
+        val sign = runBlocking {
+            signer.sign(
+                params = ConfirmParams.TransferParams.Native(
+                    AssetId(Chain.Tron),
+                    Account(Chain.Tron, from, ""),
+                    BigInteger.valueOf(10_000),
+                    DestinationAddress(from),
+                ),
+                chainData = TronSignerPreloader.TronChainData(
+                    number = 69501435,
+                    version = 31,
+                    txTrieRoot = "fe35cfe51299075978398390271d3f09bdc34e6f43fe8b76eb469314b32ceaff",
+                    witnessAddress = "41456798cb4ab28109d8cc643cd7da7bd6069ceae9",
+                    parentHash = "00000000042481fa52ef745a31aa66fc2acf8d156b6116bc5b3ede2aebbb894c",
+                    timestamp = 1739156280000,
+                    votes = emptyMap(),
+                    fee = Fee(
+                        speed = TxSpeed.Normal,
+                        feeAssetId = AssetId(Chain.Tron),
+                        amount = BigInteger.TEN
+                    ),
+                ),
+                finalAmount = BigInteger.valueOf(10_000),
+                TxSpeed.Normal,
+                privateKey.data(),
+            )
+        }
+
+        assertEquals("0x7b227261775f64617461223a7b22636f6e7472616374223a5b7b22706172616d657465722" +
+                "23a7b22747970655f75726c223a22747970652e676f6f676c65617069732e636f6d2f70726f746f6" +
+                "36f6c2e5472616e73666572436f6e7472616374222c2276616c7565223a7b22616d6f756e74223a3" +
+                "1303030302c226f776e65725f61646472657373223a2234313837623539656337626235383235303" +
+                "5333366623832333566396133663865656339633162666538222c22746f5f61646472657373223a2" +
+                "23431383762353965633762623538323530353333666238323335663961336638656563396331626" +
+                "66538227d7d2c2274797065223a225472616e73666572436f6e7472616374227d5d2c22657870697" +
+                "26174696f6e223a313733393139323238303030302c226665655f6c696d6974223a31302c2272656" +
+                "65f626c6f636b5f6279746573223a2238316662222c227265665f626c6f636b5f68617368223a226" +
+                "5333365396435303365623232653761222c2274696d657374616d70223a313733393135363238303" +
+                "030307d2c227369676e6174757265223a5b223834616231646130663934633361303434323335376" +
+                "13364363461616539376137393336633635626536303663316339373765616663623636643838633" +
+                "56666373365663338623333666363303564646662316635383836316339633235383037363030646" +
+                "238643837323137356161653533396431396430633037393663383031225d2c2274784944223a226" +
+                "53433363432633930366435656362636535656561326436653337343261393730373163383539653" +
+                "43438356535306630353166653965636337363066633933227d", sign.first().toHexString()
+        )
+    }
+
+    @Test
+    fun testTronTokenSign() {
+        val hdWallet = HDWallet(testPhrase, "")
+        val privateKey = hdWallet.getKeyForCoin(CoinType.TRON)
+        val from = hdWallet.getAddressForCoin(CoinType.TRON)
+        val signer = TronSignClient(Chain.Tron)
+
+        val sign = runBlocking {
+            signer.sign(
+                params = ConfirmParams.TransferParams.Token(
+                    AssetId(Chain.Tron, "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"),
+                    Account(Chain.Tron, from, ""),
+                    BigInteger.valueOf(10_000),
+                    DestinationAddress(from),
+                ),
+                chainData = TronSignerPreloader.TronChainData(
+                    number = 69501435,
+                    version = 31,
+                    txTrieRoot = "fe35cfe51299075978398390271d3f09bdc34e6f43fe8b76eb469314b32ceaff",
+                    witnessAddress = "41456798cb4ab28109d8cc643cd7da7bd6069ceae9",
+                    parentHash = "00000000042481fa52ef745a31aa66fc2acf8d156b6116bc5b3ede2aebbb894c",
+                    timestamp = 1739156280000,
+                    votes = emptyMap(),
+                    fee = Fee(
+                        speed = TxSpeed.Normal,
+                        feeAssetId = AssetId(Chain.Tron),
+                        amount = BigInteger.TEN
+                    ),
+                ),
+                finalAmount = BigInteger.valueOf(10_000),
+                TxSpeed.Normal,
+                privateKey.data(),
+            )
+        }
+
+        assertEquals("0x7b227261775f64617461223a7b22636f6e7472616374223a5b7b22706172616d657465722" +
+                "23a7b22747970655f75726c223a22747970652e676f6f676c65617069732e636f6d2f70726f746f6" +
+                "36f6c2e54726967676572536d617274436f6e7472616374222c2276616c7565223a7b22636f6e747" +
+                "26163745f61646472657373223a22343161363134663830336236666437383039383661343263373" +
+                "865633963376637376536646564313363222c2264617461223a22613930353963626230303030303" +
+                "03030303030303030303030303030303034313837623539656337626235383235303533336662383" +
+                "23335663961336638656563396331626665383030303030303030303030303030303030303030303" +
+                "03030303030303030303030303030303030303030303030303030303030303030303030303030323" +
+                "73130222c226f776e65725f61646472657373223a223431383762353965633762623538323530353" +
+                "33366623832333566396133663865656339633162666538227d7d2c2274797065223a22547269676" +
+                "76572536d617274436f6e7472616374227d5d2c2265787069726174696f6e223a313733393139323" +
+                "238303030302c226665655f6c696d6974223a31302c227265665f626c6f636b5f6279746573223a2" +
+                "238316662222c227265665f626c6f636b5f68617368223a226533336539643530336562323265376" +
+                "1222c2274696d657374616d70223a313733393135363238303030307d2c227369676e61747572652" +
+                "23a5b223031366238326662353533323861636331336330333866336335326631646432633866663" +
+                "73939333661343262366432303262633434646264626332623866663032373866666463346366396" +
+                "36436346566323431343666643563613233373930336262313732326565623630303233623333336" +
+                "266313733333862336130363030225d2c2274784944223a226661323932636636303631626636656" +
+                "26561643263343139393836643562323336353366306635303436323766623234613036653066343" +
+                "03562346264626566227d", sign.first().toHexString()
+        )
+    }
+
+    @Test
+    fun testDelegateSign() {
+        val hdWallet = HDWallet(testPhrase, "")
+        val privateKey = hdWallet.getKeyForCoin(CoinType.TRON)
+        val from = hdWallet.getAddressForCoin(CoinType.TRON)
+        val signer = TronSignClient(Chain.Tron)
+
+        val sign = runBlocking {
+            signer.sign(
+                params = ConfirmParams.Stake.DelegateParams(
+                    AssetId(Chain.Tron),
+                    Account(Chain.Tron, from, ""),
+                    BigInteger.valueOf(10_000),
+                    validatorId = "TCEo1hMAdaJrQmvnGTCcGT2LqrGU4N7Jqf",
+                ),
+                chainData = TronSignerPreloader.TronChainData(
+                    number = 69501435,
+                    version = 31,
+                    txTrieRoot = "fe35cfe51299075978398390271d3f09bdc34e6f43fe8b76eb469314b32ceaff",
+                    witnessAddress = "41456798cb4ab28109d8cc643cd7da7bd6069ceae9",
+                    parentHash = "00000000042481fa52ef745a31aa66fc2acf8d156b6116bc5b3ede2aebbb894c",
+                    timestamp = 1739156280000,
+                    votes = mapOf(
+                        "TLyqzVGLV1srkB7dToTAEqgDSfPtXRJZYH" to 1,
+                        "TCEo1hMAdaJrQmvnGTCcGT2LqrGU4N7Jqf" to 1,
+                    ),
+                    fee = Fee(
+                        speed = TxSpeed.Normal,
+                        feeAssetId = AssetId(Chain.Tron),
+                        amount = BigInteger.TEN
+                    ),
+                ),
+                finalAmount = BigInteger.valueOf(10_000),
+                TxSpeed.Normal,
+                privateKey.data(),
+            )
+        }
+
+        assertEquals("0x7b227261775f64617461223a7b22636f6e7472616374223a5b7b22706172616d657465722" +
+                "23a7b22747970655f75726c223a22747970652e676f6f676c65617069732e636f6d2f70726f746f6" +
+                "36f6c2e467265657a6542616c616e63655632436f6e7472616374222c2276616c7565223a7b22667" +
+                "26f7a656e5f62616c616e6365223a31303030302c226f776e65725f61646472657373223a2234313" +
+                "83762353965633762623538323530353333666238323335663961336638656563396331626665382" +
+                "22c227265736f75726365223a2242414e445749445448227d7d2c2274797065223a22467265657a6" +
+                "542616c616e63655632436f6e7472616374227d5d2c2265787069726174696f6e223a31373339313" +
+                "9323238303030302c226665655f6c696d6974223a31302c227265665f626c6f636b5f62797465732" +
+                "23a2238316662222c227265665f626c6f636b5f68617368223a22653333653964353033656232326" +
+                "53761222c2274696d657374616d70223a313733393135363238303030307d2c227369676e6174757" +
+                "265223a5b22666434333865303236306335663636383636616536363639356439396539616637383" +
+                "26462613033316434633265613638326535356635306262343539363764323434343236613130393" +
+                "73639393233376232613639613430353063303762623538653035616163383134353035623935386" +
+                "6636263343665613436303730613031225d2c2274784944223a22353366353066386539353665613" +
+                "73631633766323538303237373935666633646630343537623336393262633735363564323338663" +
+                "339313735666130366161227d", sign.first().toHexString()
+        )
+    }
+}
--- blockchain/src/main/java/com/gemwallet/android/blockchain/clients/tron/TronSignClient.kt
+++ blockchain/src/main/java/com/gemwallet/android/blockchain/clients/tron/TronSignClient.kt
@@ -2,17 +2,17 @@ package com.gemwallet.android.blockchain.clients.tron
 
 import android.text.format.DateUtils
 import com.gemwallet.android.blockchain.clients.SignClient
-import com.gemwallet.android.ext.type
 import com.gemwallet.android.math.decodeHex
+import com.gemwallet.android.model.ChainSignData
 import com.gemwallet.android.model.ConfirmParams
-import com.gemwallet.android.model.SignerParams
 import com.gemwallet.android.model.TxSpeed
 import com.google.protobuf.ByteString
-import com.wallet.core.primitives.AssetSubtype
 import com.wallet.core.primitives.Chain
 import wallet.core.java.AnySigner
 import wallet.core.jni.CoinType
 import wallet.core.jni.proto.Tron
+import wallet.core.jni.proto.Tron.TransferContract
+import wallet.core.jni.proto.Tron.TransferTRC20Contract
 import java.math.BigInteger
 
 class TronSignClient(
@@ -21,44 +21,60 @@ class TronSignClient(
 
     override fun supported(chain: Chain): Boolean = this.chain == chain
 
-    override suspend fun signTransaction(params: SignerParams, txSpeed: TxSpeed, privateKey: ByteArray): List<ByteArray> {
-        return when (params.input) {
-            is ConfirmParams.Stake.DelegateParams -> signDelegate(params, privateKey)
-            is ConfirmParams.Stake.RedelegateParams -> signRedelegate(params, privateKey)
-            is ConfirmParams.Stake.RewardsParams -> signRewards(params, privateKey)
-            is ConfirmParams.Stake.UndelegateParams -> signUndelegate(params, privateKey)
-            is ConfirmParams.Stake.WithdrawParams -> signWithdraw(params, privateKey)
-            is ConfirmParams.TokenApprovalParams,
-            is ConfirmParams.SwapParams -> throw IllegalArgumentException("Not supported")
-            is ConfirmParams.TransferParams.Native,
-            is ConfirmParams.TransferParams.Token -> signTransfer(params, txSpeed, privateKey)
-        }
+    override suspend fun sign(
+        params: ConfirmParams.TransferParams.Native,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
+        val contract = TransferContract.newBuilder().apply {
+            this.amount = finalAmount.toLong()
+            this.ownerAddress = params.from.address
+            this.toAddress = params.destination().address
+        }.build()
+        return signTransfer(chainData, contract, privateKey)
     }
 
-    private fun signTransfer(params: SignerParams, txSpeed: TxSpeed, privateKey: ByteArray): List<ByteArray> {
-        val blockInfo = params.chainData as TronSignerPreloader.TronChainData
+    override suspend fun sign(
+        params: ConfirmParams.TransferParams.Token,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
+        val contract = TransferTRC20Contract.newBuilder().apply {
+            this.contractAddress = params.assetId.tokenId!!
+            this.ownerAddress = params.from.address
+            this.toAddress = params.destination().address
+            this.amount = ByteString.copyFrom(finalAmount.toByteArray())
+        }.build()
+        return signTransfer(chainData, contract, privateKey)
+    }
+
+    private fun signTransfer(
+        chainData: TronSignerPreloader.TronChainData,
+        contract: Any,
+        privateKey: ByteArray,
+    ): List<ByteArray> {
         val transaction = Tron.Transaction.newBuilder().apply {
             this.blockHeader = Tron.BlockHeader.newBuilder().apply {
-                this.number = blockInfo.number
-                this.parentHash = ByteString.copyFrom(blockInfo.parentHash.decodeHex())
-                this.timestamp = blockInfo.timestamp
-                this.version = blockInfo.version.toInt()
-                this.witnessAddress = ByteString.copyFrom(blockInfo.witnessAddress.decodeHex())
-                this.txTrieRoot = ByteString.copyFrom(blockInfo.txTrieRoot.decodeHex())
+                this.number = chainData.number
+                this.parentHash = ByteString.copyFrom(chainData.parentHash.decodeHex())
+                this.timestamp = chainData.timestamp
+                this.version = chainData.version.toInt()
+                this.witnessAddress = ByteString.copyFrom(chainData.witnessAddress.decodeHex())
+                this.txTrieRoot = ByteString.copyFrom(chainData.txTrieRoot.decodeHex())
             }.build()
-            when (params.input.assetId.type()) {
-                AssetSubtype.NATIVE -> this.transfer = getTransferContract(params.finalAmount, params.input.from.address, params.input.destination()?.address ?: "")
-                AssetSubtype.TOKEN -> this.transferTrc20Contract = getTransferTRC20Contract(
-                    params.input.assetId.tokenId!!,
-                    params.finalAmount,
-                    params.input.from.address,
-                    params.input.destination()?.address ?: ""
-                )
-                else -> throw IllegalArgumentException("Unsupported type")
+            when (contract) {
+                is TransferTRC20Contract -> this.transferTrc20Contract = contract
+                is TransferContract -> this.transfer = contract
             }
-            this.expiration = blockInfo.timestamp + 10 * DateUtils.HOUR_IN_MILLIS
-            this.timestamp = blockInfo.timestamp
-            this.feeLimit = blockInfo.fee().amount.toLong()
+            this.expiration = chainData.timestamp + 10 * DateUtils.HOUR_IN_MILLIS
+            this.timestamp = chainData.timestamp
+            this.feeLimit = chainData.fee().amount.toLong()
         }
         val signInput = Tron.SigningInput.newBuilder().apply {
             this.transaction = transaction.build()
@@ -68,80 +84,98 @@ class TronSignClient(
         return listOf(signingOutput.json.toByteArray())
     }
 
-    fun signDelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
-
+    override suspend fun sign(
+        params: ConfirmParams.Stake.DelegateParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
         val freezContract = Tron.FreezeBalanceV2Contract.newBuilder().apply {
-            this.ownerAddress = params.input.from.address
-            this.frozenBalance = params.input.amount.toLong()
+            this.ownerAddress = params.from.address
+            this.frozenBalance = params.amount.toLong()
             this.resource = "BANDWIDTH"
         }.build()
-        val voteContract = createVoteContract(params)
+        val voteContract = createVoteContract(chainData, params.from.address)
         return listOf(
-            sign(params, freezContract, privateKey),
-            sign(params, voteContract, privateKey),
+            sign(chainData, freezContract, privateKey),
+            sign(chainData, voteContract, privateKey),
         )
     }
 
-    fun signUndelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
-        val chainData = params.chainData as TronSignerPreloader.TronChainData
+    override suspend fun sign(
+        params: ConfirmParams.Stake.UndelegateParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
         val votes = chainData.votes
         return listOf(
             Tron.UnfreezeBalanceV2Contract.newBuilder().apply {
-                this.ownerAddress = params.input.from.address
-                this.unfreezeBalance = params.input.amount.toLong()
+                this.ownerAddress = params.from.address
+                this.unfreezeBalance = params.amount.toLong()
                 this.resource = "BANDWIDTH"
             }.build(),
-            if (votes.isEmpty()) null else createVoteContract(params),
+            if (votes.isEmpty()) null else createVoteContract(chainData, params.from.address),
         )
         .filterNotNull()
         .map {
-            sign(params, it, privateKey)
+            sign(chainData, it, privateKey)
         }
     }
 
-    fun signRedelegate(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
-        val voteContract = createVoteContract(params)
-        return listOf(sign(params, voteContract, privateKey))
+    override suspend fun sign(
+        params: ConfirmParams.Stake.RedelegateParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
+        val voteContract = createVoteContract(chainData, params.from.address)
+        return listOf(sign(chainData, voteContract, privateKey))
     }
 
-    fun signRewards(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+    override suspend fun sign(
+        params: ConfirmParams.Stake.RewardsParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
         val contract = Tron.WithdrawBalanceContract.newBuilder().apply {
-            this.ownerAddress = params.input.from.address
+            this.ownerAddress = params.from.address
         }.build()
         return listOf(
-            sign(params, contract, privateKey)
+            sign(chainData, contract, privateKey)
         )
     }
 
-    fun signWithdraw(params: SignerParams, privateKey: ByteArray): List<ByteArray> {
+    override suspend fun sign(
+        params: ConfirmParams.Stake.WithdrawParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        txSpeed: TxSpeed,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val chainData = chainData as TronSignerPreloader.TronChainData
         val contract = Tron.WithdrawExpireUnfreezeContract.newBuilder().apply {
-            this.ownerAddress = params.input.from.address
+            this.ownerAddress = params.from.address
         }.build()
         return listOf(
-            sign(params, contract, privateKey)
+            sign(chainData, contract, privateKey)
         )
     }
 
-    private fun getTransferContract(value: BigInteger, ownerAddress: String, recipient: String)
-        = Tron.TransferContract.newBuilder().apply {
-            this.amount = value.toLong()
-            this.ownerAddress = ownerAddress
-            this.toAddress = recipient
-        }.build()
-
-    private fun getTransferTRC20Contract(tokenId: String, value: BigInteger, ownerAddress: String, recipient: String)
-            = Tron.TransferTRC20Contract.newBuilder().apply {
-        this.contractAddress = tokenId
-        this.ownerAddress = ownerAddress
-        this.toAddress = recipient
-        this.amount = ByteString.copyFrom(value.toByteArray())
-    }.build()
-
-    private fun createVoteContract(params: SignerParams) = Tron.VoteWitnessContract.newBuilder().apply {
-            this.ownerAddress = params.input.from.address
+    private fun createVoteContract(chainData: TronSignerPreloader.TronChainData, owner: String) = Tron.VoteWitnessContract.newBuilder().apply {
+            this.ownerAddress = owner
             this.support = true
             this.addAllVotes(
-                (params.chainData as TronSignerPreloader.TronChainData).votes.map {
+                chainData.votes.map {
                     Tron.VoteWitnessContract.Vote.newBuilder().apply {
                         this.voteAddress = it.key
                         this.voteCount = it.value
@@ -150,12 +184,11 @@ class TronSignClient(
             )
         }.build()
 
-    private fun sign(params: SignerParams, contract: Any, privateKey: ByteArray): ByteArray {
-        val blockInfo = params.chainData as TronSignerPreloader.TronChainData
+    private fun sign(chainData: TronSignerPreloader.TronChainData, contract: Any, privateKey: ByteArray): ByteArray {
         val transaction = Tron.Transaction.newBuilder().apply {
             when (contract) {
-                is Tron.TransferContract -> this.transfer = contract
-                is Tron.TransferTRC20Contract -> this.transferTrc20Contract = contract
+                is TransferContract -> this.transfer = contract
+                is TransferTRC20Contract -> this.transferTrc20Contract = contract
                 is Tron.FreezeBalanceV2Contract -> this.freezeBalanceV2 = contract
                 is Tron.VoteWitnessContract -> this.voteWitness = contract
                 is Tron.UnfreezeBalanceV2Contract -> this.unfreezeBalanceV2 = contract
@@ -163,17 +196,17 @@ class TronSignClient(
                 is Tron.WithdrawBalanceContract -> this.withdrawBalance = contract
             }
             this.blockHeader = Tron.BlockHeader.newBuilder().apply {
-                this.number = blockInfo.number
-                this.parentHash = ByteString.copyFrom(blockInfo.parentHash.decodeHex())
-                this.timestamp = blockInfo.timestamp
-                this.version = blockInfo.version.toInt()
-                this.witnessAddress = ByteString.copyFrom(blockInfo.witnessAddress.decodeHex())
-                this.txTrieRoot = ByteString.copyFrom(blockInfo.txTrieRoot.decodeHex())
+                this.number = chainData.number
+                this.parentHash = ByteString.copyFrom(chainData.parentHash.decodeHex())
+                this.timestamp = chainData.timestamp
+                this.version = chainData.version.toInt()
+                this.witnessAddress = ByteString.copyFrom(chainData.witnessAddress.decodeHex())
+                this.txTrieRoot = ByteString.copyFrom(chainData.txTrieRoot.decodeHex())
             }.build()
 
-            this.expiration = blockInfo.timestamp + 10 * DateUtils.HOUR_IN_MILLIS
-            this.timestamp = blockInfo.timestamp
-            this.feeLimit = blockInfo.fee().amount.toLong()
+            this.expiration = chainData.timestamp + 10 * DateUtils.HOUR_IN_MILLIS
+            this.timestamp = chainData.timestamp
+            this.feeLimit = chainData.fee().amount.toLong()
         }
         val signInput = Tron.SigningInput.newBuilder().apply {
             this.transaction = transaction.build()
