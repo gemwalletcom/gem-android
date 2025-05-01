70d33e88a267e0b62c18ca99bd13e91a97bad2bb
furenster <123000419+furenster@users.noreply.github.com>
2025-04-30 20:13:51 -0700

    Improve nft list

--- features/nft/presents/src/main/java/com/gemwallet/features/nft/presents/NFTItem.kt
+++ features/nft/presents/src/main/java/com/gemwallet/features/nft/presents/NFTItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Card
@@ -11,10 +12,18 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil3.transform.RoundedCornersTransformation
 import com.gemwallet.android.ui.components.designsystem.padding16
 import com.gemwallet.android.ui.components.designsystem.padding8
 import com.gemwallet.android.ui.components.image.AsyncImage
@@ -28,11 +37,12 @@ fun NFTItem(
     collectionIdAction: NftCollectionIdAction,
     assetIdAction: NftAssetIdAction,
 ) {
+    var imgHeight by remember { mutableStateOf(0.dp) }
+    val density = LocalDensity.current
     Card(
         modifier = Modifier
             .clickable(onClick = { model.onClick(collectionIdAction, assetIdAction) })
-            .padding(start = padding8, bottom = padding16, end = padding8)
-        ,
+            .padding(start = padding8, bottom = padding16, end = padding8),
         colors = CardDefaults.cardColors().copy(containerColor = MaterialTheme.colorScheme.background)
     ) {
         Column(
@@ -41,14 +51,17 @@ fun NFTItem(
             AsyncImage(
                 model.imageUrl,
                 placeholderText = model.name,
-                transformation = null,
+                transformation = RoundedCornersTransformation(24f, 24f, 24f, 24f),
                 size = null,
-                modifier = Modifier.widthIn(min = 150.dp).height(150.dp)
+                modifier = Modifier
+                    .widthIn(min = 150.dp)
+                    .height(imgHeight)
+                    .onGloballyPositioned { imgHeight = with(density) { it.size.width.toDp() } }
             )
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
-                text = model.name
+                text = model.name,
             )
         }
     }
