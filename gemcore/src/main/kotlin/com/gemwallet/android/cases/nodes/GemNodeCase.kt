57446cf7aecfa01fcc568d9f8c730ea6116cabca
furenster <123000419+furenster@users.noreply.github.com>
2025-10-04 01:33:25 -0700

    Change node domain

--- gemcore/src/main/kotlin/com/gemwallet/android/cases/nodes/GemNodeCase.kt
+++ gemcore/src/main/kotlin/com/gemwallet/android/cases/nodes/GemNodeCase.kt
@@ -4,7 +4,7 @@ import com.wallet.core.primitives.Chain
 import com.wallet.core.primitives.Node
 import com.wallet.core.primitives.NodeState
 
-fun getGemNodeUrl(chain: Chain) = "https://${chain.string}.gemnodes.com"
+fun getGemNodeUrl(chain: Chain) = "https://gemnodes.com/${chain.string}"
 
 fun getGemNode(chain: Chain) = Node(
     url = getGemNodeUrl(chain),
