e50d183cad733453a01fc160c0a6486ca95351d7
furenster <123000419+furenster@users.noreply.github.com>
2025-10-21 23:11:56 -0700

    Clean imports

--- app/src/main/kotlin/com/gemwallet/android/ui/navigation/WalletNavGraph.kt
+++ app/src/main/kotlin/com/gemwallet/android/ui/navigation/WalletNavGraph.kt
@@ -1,6 +1,5 @@
 package com.gemwallet.android.ui.navigation
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
