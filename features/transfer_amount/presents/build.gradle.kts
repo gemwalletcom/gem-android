2326c7ed9db4e873100b5ddc5d3e1083dcf3b390
furenster <123000419+furenster@users.noreply.github.com>
2025-11-14 19:55:52 -0800

    Bump libraries

--- features/transfer_amount/presents/build.gradle.kts
+++ features/transfer_amount/presents/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
 
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
-    implementation(libs.androidx.compose.material3)
     kapt(libs.hilt.compiler)
 
     debugImplementation(libs.androidx.ui.tooling)
