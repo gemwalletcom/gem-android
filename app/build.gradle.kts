cdf5563082b07d8d818c02a1ace50316f75d805d
furenster <123000419+furenster@users.noreply.github.com>
2025-12-17 18:32:20 -0800

    Bump to 1.3.65 (659)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.gemwallet.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 658
-        versionName = "1.3.64"
+        versionCode = 659
+        versionName = "1.3.65"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
