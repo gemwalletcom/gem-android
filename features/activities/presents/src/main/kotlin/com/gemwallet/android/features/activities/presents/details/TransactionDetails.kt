6d00e07d2d638a3093c725c04872029a63167487
furenster <123000419+furenster@users.noreply.github.com>
2025-12-22 19:19:16 -0800

    Add transaction sharing

--- features/activities/presents/src/main/kotlin/com/gemwallet/android/features/activities/presents/details/TransactionDetails.kt
+++ features/activities/presents/src/main/kotlin/com/gemwallet/android/features/activities/presents/details/TransactionDetails.kt
@@ -1,13 +1,16 @@
 package com.gemwallet.android.features.activities.presents.details
 
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -54,12 +57,30 @@ fun TransactionDetails(
     val uiState by viewModel.screenModel.collectAsStateWithLifecycle()
     val model = uiState
     var isShowFeeDetails by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    val onShare = fun () {
+        val type = "text/plain"
+        val subject = uiState?.explorerUrl
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = type
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, subject)
+
+        context.startActivity(Intent.createChooser(intent, uiState?.explorerName))
+    }
 
     if (model == null) {
         LoadingScene(title = "", onCancel)
     } else {
         Scene(
             title = model.type.getTransactionTitle(model.direction, model.state),
+            actions = {
+                IconButton(onShare) {
+                    Icon(Icons.Default.Share, "")
+                }
+            },
             onClose = onCancel,
         ) {
             LazyColumn(modifier = Modifier.fillMaxSize()) {
