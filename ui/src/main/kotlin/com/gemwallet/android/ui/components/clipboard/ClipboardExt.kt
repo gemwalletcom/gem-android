f50f64dea2d3a89a735d3c6b4df7b6aac59f44eb
furenster <123000419+furenster@users.noreply.github.com>
2025-12-27 08:27:36 -0800

    Clean clipboard

--- ui/src/main/kotlin/com/gemwallet/android/ui/components/clipboard/ClipboardExt.kt
+++ ui/src/main/kotlin/com/gemwallet/android/ui/components/clipboard/ClipboardExt.kt
@@ -2,11 +2,14 @@ package com.gemwallet.android.ui.components.clipboard
 
 import android.content.ClipData
 import android.content.ClipDescription
+import android.content.ClipboardManager
 import android.content.Context
 import android.os.Build
 import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.compose.ui.platform.NativeClipboard
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 fun NativeClipboard.setPlainText(context: Context, data: String, isSensitive: Boolean = false) {
     val clip = ClipData.newPlainText("", data).apply {
@@ -22,11 +25,10 @@ fun NativeClipboard.setPlainText(context: Context, data: String, isSensitive: Bo
     }
     setPrimaryClip(clip)
 
-// No plans to support this for now
-//    Executors.newSingleThreadScheduledExecutor().schedule({
-//        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//        clipboard.clearPrimaryClip()
-//    }, 30, TimeUnit.SECONDS)
+    Executors.newSingleThreadScheduledExecutor().schedule({
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.clearPrimaryClip()
+    }, 1, TimeUnit.MINUTES)
 
     if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
         Toast.makeText(context, "Copied", Toast.LENGTH_SHORT).show()
