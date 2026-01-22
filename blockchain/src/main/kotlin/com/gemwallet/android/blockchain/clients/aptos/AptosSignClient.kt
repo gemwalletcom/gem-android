f39251cefe2b05ad2a4056d6e4a5557744f9b1cc
furenster <123000419+furenster@users.noreply.github.com>
2026-01-21 20:36:07 -0800

    Add stake signing

--- blockchain/src/main/kotlin/com/gemwallet/android/blockchain/clients/aptos/AptosSignClient.kt
+++ blockchain/src/main/kotlin/com/gemwallet/android/blockchain/clients/aptos/AptosSignClient.kt
@@ -2,14 +2,18 @@ package com.gemwallet.android.blockchain.clients.aptos
 
 import com.gemwallet.android.blockchain.clients.SignClient
 import com.gemwallet.android.blockchain.services.mapper.toGem
+import com.gemwallet.android.domains.asset.chain
 import com.gemwallet.android.domains.confirm.toGem
+import com.gemwallet.android.domains.stake.toGem
 import com.gemwallet.android.model.ChainSignData
 import com.gemwallet.android.model.ConfirmParams
 import com.gemwallet.android.model.Fee
 import com.gemwallet.android.model.GasFee
 import com.wallet.core.primitives.Chain
+import com.wallet.core.primitives.StakeType
 import uniffi.gemstone.GemChainSigner
 import uniffi.gemstone.GemGasPriceType
+import uniffi.gemstone.GemStakeType
 import uniffi.gemstone.GemTransactionInputType
 import uniffi.gemstone.GemTransactionLoadInput
 import java.math.BigInteger
@@ -81,6 +85,69 @@ class AptosSignClient(
             metadata = metadata,
         )
         return gemSigner.signSwap(gemLoadInput, privateKey).map { it.toByteArray() }
+    }
+
+    override suspend fun signDelegate(
+        params: ConfirmParams.Stake.DelegateParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        fee: Fee,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val metadata = (chainData as AptosChainData).toGem()
+        val gemLoadInput = GemTransactionLoadInput(
+            inputType = GemTransactionInputType.Stake(params.asset.toGem(), GemStakeType.Delegate(params.validator.toGem(params.asset.chain.string))),
+            senderAddress = params.from.address,
+            destinationAddress = params.destination()?.address ?: throw Exception("Incorrect destination"),
+            value = finalAmount.toString(),
+            gasPrice = GemGasPriceType.Regular((fee as? GasFee)?.maxGasPrice.toString()),
+            memo = params.memo(),
+            isMaxValue = params.useMaxAmount,
+            metadata = metadata,
+        )
+        return gemSigner.signStake(gemLoadInput, privateKey).map { it.toByteArray() }
+    }
+
+    override suspend fun signUndelegate(
+        params: ConfirmParams.Stake.UndelegateParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        fee: Fee,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val metadata = (chainData as AptosChainData).toGem()
+        val gemLoadInput = GemTransactionLoadInput(
+            inputType = GemTransactionInputType.Stake(params.asset.toGem(), GemStakeType.Undelegate(params.delegation.toGem(params.asset.chain.string))),
+            senderAddress = params.from.address,
+            destinationAddress = params.destination()?.address ?: throw Exception("Incorrect destination"),
+            value = finalAmount.toString(),
+            gasPrice = GemGasPriceType.Regular((fee as? GasFee)?.maxGasPrice.toString()),
+            memo = params.memo(),
+            isMaxValue = params.useMaxAmount,
+            metadata = metadata,
+        )
+        return gemSigner.signStake(gemLoadInput, privateKey).map { it.toByteArray() }
+    }
+
+    override suspend fun signWithdraw(
+        params: ConfirmParams.Stake.WithdrawParams,
+        chainData: ChainSignData,
+        finalAmount: BigInteger,
+        fee: Fee,
+        privateKey: ByteArray
+    ): List<ByteArray> {
+        val metadata = (chainData as AptosChainData).toGem()
+        val gemLoadInput = GemTransactionLoadInput(
+            inputType = GemTransactionInputType.Stake(params.asset.toGem(), GemStakeType.Withdraw(params.delegation.toGem(params.asset.chain.string  ))),
+            senderAddress = params.from.address,
+            destinationAddress = params.destination()?.address ?: throw Exception("Incorrect destination"),
+            value = finalAmount.toString(),
+            gasPrice = GemGasPriceType.Regular((fee as? GasFee)?.maxGasPrice.toString()),
+            memo = params.memo(),
+            isMaxValue = params.useMaxAmount,
+            metadata = metadata,
+        )
+        return gemSigner.signStake(gemLoadInput, privateKey).map { it.toByteArray() }
     }
 
     override fun supported(chain: Chain): Boolean = this.chain == chain
