da74a7c5d26413bcf7178b8a67e1e123d33d910e
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-03-10 16:46:20 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,19 +54,6 @@ allprojects {
             }
         }
     }
-
-    dependencyLocking {
-        lockAllConfigurations()
-    }
-}
-
-subprojects {
-    dependencyLocking {
-        lockAllConfigurations()
-    }
-    configurations.configureEach {
-        resolutionStrategy.activateDependencyLocking()
-    }
 }
 
 tasks.register("clean", Delete::class) {
